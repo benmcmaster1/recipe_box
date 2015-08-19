346d817cb8162bab8bde94dfacf0e67c7a1dc10a
benmcmaster <ben_mcmaster@outlook.com>
2015-08-19 06:55:14 +0000

    Change user edit permissions and navbar email display

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,18 @@
 class Recipe < ActiveRecord::Base
-    belongs_to :user
-    has_many :ingredients
-    has_many :directions
-    
-    # accepts nested attribues for ingredients/directions, but it it's blank, it's not going to save.
-    accepts_nested_attributes_for :ingredients,
-                                  reject_if: proc { |attributes| attributes['name'].blank?},
-                                  allow_destroy: true
-    accepts_nested_attributes_for :directions,
-                                  reject_if: proc { |attributes| attributes['name'].blank?},
-                                  allow_destroy: true
-    
-    
-    validates :title, :description, :image, presence: true
-    has_attached_file :image, styles: { :medium => "400x400#"}, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	belongs_to :user
+
+	has_many :ingredients
+	has_many :directions
+
+	accepts_nested_attributes_for :ingredients,
+  															reject_if: proc { |attributes| attributes['name'].blank? },
+  															allow_destroy: true
+ 	accepts_nested_attributes_for :directions,
+  															reject_if: proc { |attributes| attributes['step'].blank? },
+  															allow_destroy: true
+
+  validates :title, :description, :image, presence: true
+
+	has_attached_file :image, styles: { :medium => "400x400#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
